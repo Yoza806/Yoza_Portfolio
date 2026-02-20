a347be74b02b76fd8ebea697ae007f1dd559bdc6
Yoza <yoza@email.com>
2026-02-20 23:35:55 +0530

    Secure:True change

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { 
-        secure: false, // Set to true if using HTTPS
+        secure: true, // Set to true if using HTTPS
         maxAge: 24 * 60 * 60 * 1000 // 24 hours
     }
 }));
