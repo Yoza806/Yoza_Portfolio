b6567e20cfb63ce7975e6d3669bf464eb96763e6
Yoza <yoza@email.com>
2026-02-20 11:10:29 +0530

    updated db for render

--- index.js
+++ index.js
@@ -11,15 +11,23 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-const db = new pg.Client(
-    {
-        user:process.env.DB_USER,
-        host:process.env.DB_HOST,
-        database:process.env.DB_NAME,
-        password:process.env.DB_PASSWORD,
-        port:process.env.DB_PORT,
-    }
-);
+// const db = new pg.Client(
+//     {
+//         user:process.env.DB_USER,
+//         host:process.env.DB_HOST,
+//         database:process.env.DB_NAME,
+//         password:process.env.DB_PASSWORD,
+//         port:process.env.DB_PORT,
+//     }
+// );
+
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 db.connect();
 
 
