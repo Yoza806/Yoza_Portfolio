c05b4f76a6b06b18e6d638f095f18b79f45bbc4c
Yoza <yoza@email.com>
2026-02-20 19:27:04 +0530

    temp remove db

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const port = process.env.PORT;
 //     }
 // );
 
-const connectionString = process.env.DATABASE_URL
-const sql = postgres(connectionString)
+// const connectionString = process.env.DATABASE_URL
+// const sql = postgres(connectionString)
 
-export default sql
+// export default sql
 
 
 //middleware 
@@ -52,10 +52,16 @@ app.set("view engine","ejs")
 app.get("/", async (req,res)=>{
 
     try{
-        const result = await db.query("SELECT * FROM projects ORDER BY created_at DESC");
-        const book_result = await db.query("SELECT * FROM books ORDER BY created_at DESC")
-        const gallery_result = await db.query("SELECT * FROM gallery ORDER BY created_at DESC")
-        const skills_Result = await db.query("SELECT * FROM skills ORDER BY created_at DESC");
+        // const result = await db.query("SELECT * FROM projects ORDER BY created_at DESC");
+        // const book_result = await db.query("SELECT * FROM books ORDER BY created_at DESC")
+        // const gallery_result = await db.query("SELECT * FROM gallery ORDER BY created_at DESC")
+        // const skills_Result = await db.query("SELECT * FROM skills ORDER BY created_at DESC");
+
+        const result = { rows: [] };
+        const book_result = { rows: [] };
+        const gallery_result = { rows: [] };
+        const skills_Result = { rows: [] };
+
         res.render("index.ejs",{projects:result.rows, books:book_result.rows,gallery:gallery_result.rows, skills:skills_Result.rows});
     }catch(err){
         console.log(err);
